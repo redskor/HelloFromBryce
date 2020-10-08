f923df06e11a5f08958d99bf3d91d511aca017b9
redskor <72470482+redskor@users.noreply.github.com>
2020-10-08 15:13:05 -0600

    These were the most recent changes

--- src/carbee/Main.java
+++ src/carbee/Main.java
@@ -4,7 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Bryce!");
-        System.out.println("bryce wuz here");
-        System.out.println("lulz");
     }
 }
